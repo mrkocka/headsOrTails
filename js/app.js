40b49838058740f8010d81950a1303a49d6e4655
Mr. Joe <radics.pepe@gmail.com>
2022-05-31 21:56:05 +0200

    save_1_05.31

--- js/app.js
+++ js/app.js
@@ -1,31 +1,28 @@
-let button = document.getElementById("js-button");
+let button = document.getElementById("button");
 
 button.addEventListener("click", loade);
 
 function loade() {
-  document.getElementById("js-no").style.display = "none";
-  document.getElementById("js-yes").style.display = "none";
-  document.getElementById("js-loade").style.display = "block";
-  setTimeout(delet, 1500);
+  document.getElementById("no").classList.remove("visit");
+  document.getElementById("yes").classList.remove("visit");
+  document.getElementById("load").classList.toggle("visit");
+  setTimeout(clear, 1500);
+}
+function clear() {
+  document.getElementById("load").classList.toggle("visit");
+  setTimeout(fate, 300);
+}
 
-  function delet() {
-    document.getElementById("js-loade").style.display = "none";
+function fate() {
+  let random = Math.floor(Math.random() * 2);
 
-    setTimeout(fate, 300);
+  if (random === 0) {
+    document.getElementById("no").classList.toggle("visit");
+    console.log("NO");
   }
 
-  function fate() {
-    let random = Math.floor(Math.random() * 2);
-    console.log(random);
-
-    if (random === 0) {
-      document.getElementById("js-no").style.display = "block";
-      console.log("NO");
-    }
-
-    if (random === 1) {
-      document.getElementById("js-yes").style.display = "block";
-      console.log("YES");
-    }
+  if (random === 1) {
+    document.getElementById("yes").classList.toggle("visit");
+    console.log("YES");
   }
 }
